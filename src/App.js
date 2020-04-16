93e70bcc790d717ab7884083b18cb1ee82e8c241
Jens Duray <jensduray@jenss-mbp.localdomain>
2020-04-16 22:48:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="page">
+        <span className="menu_toggle">
+          <i className="menu_open fa fa-bars fa-lg"></i>
+          <i className="menu_close fa fa-times fa-lg"></i>
+        </span>
+        <ul className="menu_items">
+          <li><a href="#"><i className="icon fa fa-signal fa-2x"></i> Moar</a></li>
+          <li><a href="#"><i className="icon fa fa-coffee fa-2x"></i> Coffee</a></li>
+          <li><a href="#"><i className="icon fa fa-heart fa-2x"></i> Please</a></li>
+        </ul>
+        <main className="content">
+    <div className="content_inner">
+      <p>TEXT</p>
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +33,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header> */}
+      </div>
+      </main>
+      </div>
     </div>
   );
 }
